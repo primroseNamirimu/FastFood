de29e308f4f6b38df1b1080a8abd565017fe3f99
Your Name <you@example.com>
2022-02-21 14:32:18 +0300

    changes

--- app/Http/Controllers/adminReportController.php
+++ app/Http/Controllers/adminReportController.php
@@ -19,7 +19,7 @@ class AdminReportController extends Controller
         ->join('food','food.id','=','food_order.food_id')
         ->join('orders','orders.id','=','food_order.order_id')
         ->join('users','users.id','=','orders.user_id')
-        ->select('food_order.order_id', DB::raw('SUM(food.price) as total'),'orders.created_at','users.id','users.lastname','users.firstname')
+        ->select('food_order.order_id', DB::raw('SUM(food.price) as total'),'orders.created_at','users.lastname','users.firstname')
         ->groupBy('order_id','orders.created_at','users.lastname','users.firstname')
        
         ->whereMonth('food_order.created_at',date('m'))->get();
